0aadadf92570e35cba41218f9c370d568e36cc6b
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-02 14:03:06 +0200

    Legacy check migration: perle_chassis II
    Change-Id: Ib4b85b67e5fb36d8eacff3c85487c672dbcdfbb5

--- cmk/plugins/perle/agent_based/perle_chassis.py
+++ cmk/plugins/perle/agent_based/perle_chassis.py
@@ -7,13 +7,21 @@ from dataclasses import dataclass
 
 from cmk.agent_based.v2 import (
     Attributes,
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
     InventoryPlugin,
     InventoryResult,
+    Result,
+    Service,
     SimpleSNMPSection,
     SNMPTree,
+    State,
     StringTable,
 )
-from cmk.plugins.perle.lib import DETECT_PERLE
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
+from cmk.plugins.perle.lib import DETECT_PERLE, perle_check_alarms
 
 
 @dataclass
@@ -58,6 +66,55 @@ snmp_section_perle_chassis = SimpleSNMPSection(
         ],
     ),
     detect=DETECT_PERLE,
+)
+
+
+_MAP_DIAG_STATES = {
+    "0": (State.OK, "passed"),
+    "1": (State.WARN, "firmware download required"),
+    "2": (State.CRIT, "temperature sensor not functional"),
+}
+
+
+def discover_perle_chassis(section: _Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check_perle_chassis(section: _Section) -> CheckResult:
+    state, state_readable = _MAP_DIAG_STATES[section.diagnosis_state]
+    yield Result(state=state, summary=f"Diagnostic result: {state_readable}")
+    yield perle_check_alarms(section.alarms)
+
+
+check_plugin_perle_chassis = CheckPlugin(
+    name="perle_chassis",
+    service_name="Chassis status",
+    discovery_function=discover_perle_chassis,
+    check_function=check_perle_chassis,
+)
+
+
+def discover_perle_chassis_temp(section: _Section) -> DiscoveryResult:
+    yield Service(item="chassis")
+
+
+def check_perle_chassis_temp(item: str, params: TempParamType, section: _Section) -> CheckResult:
+    yield from check_temperature(
+        reading=section.temp,
+        params=params,
+        unique_name="perle_chassis_temp",
+        value_store=get_value_store(),
+    )
+
+
+check_plugin_perle_chassis_temp = CheckPlugin(
+    name="perle_chassis_temp",
+    sections=["perle_chassis"],
+    service_name="Temperature %s",
+    discovery_function=discover_perle_chassis_temp,
+    check_function=check_perle_chassis_temp,
+    check_ruleset_name="temperature",
+    check_default_parameters={},
 )
 
 
