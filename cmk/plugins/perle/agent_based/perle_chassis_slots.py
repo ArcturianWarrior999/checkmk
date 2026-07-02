0ac58a697e78fb20a16be9247bc4decbe8fde013
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-02 14:08:10 +0200

    Legacy check migration: perle_chassis_slots II
    Change-Id: I893abb9af882bc7accda100488780e06627496bc

--- cmk/plugins/perle/agent_based/perle_chassis_slots.py
+++ cmk/plugins/perle/agent_based/perle_chassis_slots.py
@@ -26,14 +26,20 @@
 
 
 from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
     InventoryPlugin,
     InventoryResult,
+    Result,
+    Service,
     SimpleSNMPSection,
     SNMPTree,
+    State,
     StringTable,
     TableRow,
 )
-from cmk.plugins.perle.lib import DETECT_PERLE
+from cmk.plugins.perle.lib import DETECT_PERLE, perle_check_alarms
 
 Section = StringTable
 
@@ -61,6 +67,57 @@ snmp_section_perle_chassis_slots = SimpleSNMPSection(
     ),
     detect=DETECT_PERLE,
     parse_function=parse_perle_chassis_slots,
+)
+
+
+_MAP_DIAG_STATES = {
+    "0": (State.OK, "passed"),
+    "1": (State.CRIT, "media converter module's PHY is not functional"),
+    "2": (State.WARN, "firmware download required"),
+}
+
+
+def discover_perle_chassis_slots(section: Section) -> DiscoveryResult:
+    for (
+        index,
+        _name,
+        _modelname,
+        _serial,
+        _bootloader,
+        _fw,
+        _alarms,
+        _diagstate,
+        ty,
+        _descr,
+    ) in section:
+        if ty != "0":
+            yield Service(item=index)
+
+
+def check_perle_chassis_slots(item: str, section: Section) -> CheckResult:
+    for (
+        index,
+        name,
+        _modelname,
+        _serial,
+        _bootloader,
+        _fw,
+        alarms_str,
+        diagstate,
+        _ty,
+        _descr,
+    ) in section:
+        if item == index:
+            state, state_readable = _MAP_DIAG_STATES[diagstate]
+            yield Result(state=state, summary=f"[{name}] Diagnostic result: {state_readable}")
+            yield perle_check_alarms(alarms_str)
+
+
+check_plugin_perle_chassis_slots = CheckPlugin(
+    name="perle_chassis_slots",
+    service_name="Chassis status slot %s",
+    discovery_function=discover_perle_chassis_slots,
+    check_function=check_perle_chassis_slots,
 )
 
 
