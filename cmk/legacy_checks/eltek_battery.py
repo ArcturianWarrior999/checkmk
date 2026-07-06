2e5b740b51d66eca1502a4dff3f72eb284288baf
Moritz Kiemer <moritz.kiemer@checkmk.com>
2026-07-06 16:40:11 +0200

    Legacy check migration: eltek_battery I
    Change-Id: I6d4fa782e72582b16178c22c952576323f1d3e09

--- cmk/legacy_checks/eltek_battery.py
+++ cmk/legacy_checks/eltek_battery.py
@@ -3,21 +3,57 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-def"
+from collections.abc import Mapping
+from dataclasses import dataclass
+from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPTree
-from cmk.legacy_includes.elphase import check_elphase
-from cmk.legacy_includes.temperature import check_temperature
+from cmk.agent_based.v2 import (
+    CheckPlugin,
+    CheckResult,
+    DiscoveryResult,
+    get_value_store,
+    Result,
+    Service,
+    SimpleSNMPSection,
+    SNMPTree,
+    State,
+    StringTable,
+)
 from cmk.plugins.eltek.lib import DETECT_ELTEK
-
-check_info = {}
+from cmk.plugins.lib.elphase import check_elphase, ElPhase
+from cmk.plugins.lib.temperature import check_temperature, TempParamType
 
 # .1.3.6.1.4.1.12148.9.3.1.0 --> ELTEK-DISTRIBUTED-MIB::batteryName.0
 # .1.3.6.1.4.1.12148.9.3.2.0 5485 --> ELTEK-DISTRIBUTED-MIB::batteryVoltage.0
 # .1.3.6.1.4.1.12148.9.3.3.0 0 --> ELTEK-DISTRIBUTED-MIB::batteryCurrent.0
 # .1.3.6.1.4.1.12148.9.3.4.0 19 --> ELTEK-DISTRIBUTED-MIB::batteryTemp.0
 # .1.3.6.1.4.1.12148.9.3.5.0 0 --> ELTEK-DISTRIBUTED-MIB::batteryBreakerStatus.0
+
+
+@dataclass(frozen=True)
+class Section:
+    supply: Mapping[str, ElPhase]
+    temp: float
+    breaker: str
+
+
+def parse_eltek_battery(string_table: StringTable) -> Section | None:
+    if not string_table:
+        return None
+    voltage, current, temp, breaker_status = string_table[0]
+    return Section(
+        supply={
+            "Supply": ElPhase.from_dict(
+                {
+                    "voltage": float(voltage) / 100,
+                    "current": float(current),
+                }
+            )
+        },
+        temp=float(temp),
+        breaker=breaker_status,
+    )
+
 
 #   .--breaker status------------------------------------------------------.
 #   |   _                    _                   _        _                |
@@ -31,40 +67,20 @@ check_info = {}
 #   '----------------------------------------------------------------------'
 
 
-def parse_eltek_battery(string_table):
-    if not string_table:
-        return None
-    voltage, current, temp, breaker_status = string_table[0]
-    return {
-        "supply": {
-            "Supply": {
-                "voltage": float(voltage) / 100,
-                "current": float(current),
-            }
-        },
-        "temp": float(temp),
-        "breaker": breaker_status,
+def discover_eltek_battery(section: Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check_eltek_battery(section: Section) -> CheckResult:
+    map_status = {
+        "0": (State.OK, "normal"),
+        "1": (State.CRIT, "alarm"),
     }
+    state, state_readable = map_status[section.breaker]
+    yield Result(state=state, summary=f"Status: {state_readable}")
 
 
-def discover_eltek_battery(parsed):
-    if "breaker" in parsed:
-        return [(None, None)]
-    return []
-
-
-def check_eltek_battery(_no_item, _no_params, parsed):
-    if "breaker" in parsed:
-        map_status = {
-            "0": (0, "normal"),
-            "1": (2, "alarm"),
-        }
-        state, state_readable = map_status[parsed["breaker"]]
-        return state, "Status: %s" % state_readable
-    return None
-
-
-check_info["eltek_battery"] = LegacyCheckDefinition(
+snmp_section_eltek_battery = SimpleSNMPSection(
     name="eltek_battery",
     detect=DETECT_ELTEK,
     fetch=SNMPTree(
@@ -72,12 +88,16 @@ check_info["eltek_battery"] = LegacyCheckDefinition(
         oids=["2", "3", "4", "5"],
     ),
     parse_function=parse_eltek_battery,
+)
+
+
+check_plugin_eltek_battery = CheckPlugin(
+    name="eltek_battery",
     service_name="Battery Breaker Status",
     discovery_function=discover_eltek_battery,
     check_function=check_eltek_battery,
 )
 
-# .
 #   .--temperature---------------------------------------------------------.
 #   |      _                                      _                        |
 #   |     | |_ ___ _ __ ___  _ __   ___ _ __ __ _| |_ _   _ _ __ ___       |
@@ -90,20 +110,21 @@ check_info["eltek_battery"] = LegacyCheckDefinition(
 # suggested by customer
 
 
-def discover_eltek_battery_temp(parsed):
-    if "temp" in parsed:
-        return [("Battery", {})]
-    return []
+def discover_eltek_battery_temp(section: Section) -> DiscoveryResult:
+    yield Service(item="Battery")
 
 
-def check_eltek_battery_temp(item, params, parsed):
+def check_eltek_battery_temp(item: str, params: TempParamType, section: Section) -> CheckResult:
     # For temp checks we need an item but we have only one
-    if "temp" in parsed:
-        return check_temperature(parsed["temp"], params, "eltek_battery_temp_Battery")
-    return None
+    yield from check_temperature(
+        section.temp,
+        params,
+        unique_name="eltek_battery_temp_Battery",
+        value_store=get_value_store(),
+    )
 
 
-check_info["eltek_battery.temp"] = LegacyCheckDefinition(
+check_plugin_eltek_battery_temp = CheckPlugin(
     name="eltek_battery_temp",
     service_name="Temperature %s",
     sections=["eltek_battery"],
@@ -115,7 +136,6 @@ check_info["eltek_battery.temp"] = LegacyCheckDefinition(
     },
 )
 
-# .
 #   .--phase---------------------------------------------------------------.
 #   |                           _                                          |
 #   |                     _ __ | |__   __ _ ___  ___                       |
@@ -126,15 +146,18 @@ check_info["eltek_battery.temp"] = LegacyCheckDefinition(
 #   '----------------------------------------------------------------------'
 
 
-def discover_eltek_battery_supply(section):
-    yield from ((item, {}) for item in section["supply"])
+def discover_eltek_battery_supply(section: Section) -> DiscoveryResult:
+    yield from (Service(item=item) for item in section.supply)
 
 
-def check_eltek_battery_supply(item, params, parsed):
-    return check_elphase(item, params, parsed["supply"])
+def check_eltek_battery_supply(
+    item: str, params: Mapping[str, Any], section: Section
+) -> CheckResult:
+    if phase := section.supply.get(item):
+        yield from check_elphase(params, phase)
 
 
-check_info["eltek_battery.supply"] = LegacyCheckDefinition(
+check_plugin_eltek_battery_supply = CheckPlugin(
     name="eltek_battery_supply",
     service_name="Battery %s",
     sections=["eltek_battery"],
