8e29c51ca27211858b813ce428f40bb14974609c
Matthew Rindel <matthew.rindel@checkmk.com>
2026-07-09 10:18:02 +0200

    Legacy check migration: cisco_nexus_cpu I
    CMK-33604
    
    Change-Id: I0ed7b3f6378f66d241ff651ae96775b76893e04c

--- cmk/legacy_checks/cisco_nexus_cpu.py
+++ cmk/legacy_checks/cisco_nexus_cpu.py
@@ -3,50 +3,59 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-# mypy: disable-error-code="no-untyped-call"
-
-from collections.abc import Iterator, Mapping
+import time
+from collections.abc import Mapping
+from dataclasses import dataclass
 from typing import Any
 
-from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
 from cmk.agent_based.v2 import (
     all_of,
+    CheckPlugin,
+    CheckResult,
     contains,
     DiscoveryResult,
     exists,
+    get_value_store,
     Service,
+    SimpleSNMPSection,
     SNMPTree,
     StringTable,
 )
-from cmk.legacy_includes.cpu_util import check_cpu_util
-
-check_info = {}
+from cmk.plugins.lib.cpu_util import check_cpu_util
 
 # .1.3.6.1.4.1.9.9.305.1.1.1.0 1 --> CISCO-SYSTEM-EXT-MIB::cseSysCPUUtilization.0
 
 
-def discover_cisco_nexus_cpu(section: StringTable) -> DiscoveryResult:
-    if section and section[0][0]:
-        yield Service()
+@dataclass(frozen=True)
+class Section:
+    cpu_perc: float
 
 
-def check_cisco_nexus_cpu(
-    _no_item: None, params: Mapping[str, Any], info: StringTable
-) -> Iterator[tuple[int, str, list[Any]]]:
-    yield from check_cpu_util(float(info[0][0]), params)
+def discover_cisco_nexus_cpu(section: Section) -> DiscoveryResult:
+    yield Service()
+
+
+def check_cisco_nexus_cpu(params: Mapping[str, Any], section: Section) -> CheckResult:
+    yield from check_cpu_util(
+        util=section.cpu_perc,
+        params=params,
+        value_store=get_value_store(),
+        this_time=time.time(),
+    )
 
 
 # Migration NOTE: Create a separate section, but a common check plug-in for
 # tplink_cpu, hr_cpu, cisco_nexus_cpu, bintec_cpu, winperf_processor,
 # lxc_container_cpu, docker_container_cpu.
 # Migration via cmk/update_config.py!
-def parse_cisco_nexus_cpu(string_table: StringTable) -> StringTable:
-    return string_table
+def parse_cisco_nexus_cpu(string_table: StringTable) -> Section | None:
+    if not string_table or not string_table[0][0]:
+        return None
+    return Section(float(string_table[0][0]))
 
 
-check_info["cisco_nexus_cpu"] = LegacyCheckDefinition(
+snmp_section_cisco_nexus_cpu = SimpleSNMPSection(
     name="cisco_nexus_cpu",
-    parse_function=parse_cisco_nexus_cpu,
     detect=all_of(
         contains(".1.3.6.1.2.1.1.1.0", "cisco"),
         contains(".1.3.6.1.2.1.1.1.0", "nx-os"),
@@ -56,6 +65,12 @@ check_info["cisco_nexus_cpu"] = LegacyCheckDefinition(
         base=".1.3.6.1.4.1.9.9.305.1.1.1",
         oids=["0"],
     ),
+    parse_function=parse_cisco_nexus_cpu,
+)
+
+
+check_plugin_cisco_nexus_cpu = CheckPlugin(
+    name="cisco_nexus_cpu",
     service_name="CPU utilization",
     discovery_function=discover_cisco_nexus_cpu,
     check_function=check_cisco_nexus_cpu,
--- tests/unit/cmk/plugins/collection/agent_based/test_cisco_detection.py
+++ tests/unit/cmk/plugins/collection/agent_based/test_cisco_detection.py
@@ -9,9 +9,10 @@ import pytest
 
 from cmk.agent_based.internal import evaluate_snmp_detection
 from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
-from cmk.agent_based.v2 import SNMPSection, StringTable
+from cmk.agent_based.v2 import SimpleSNMPSection, SNMPSection, StringTable
 from cmk.legacy_checks.cisco_cpu import check_info as cisco_cpu_check_info
-from cmk.legacy_checks.cisco_nexus_cpu import check_info as cisco_nexus_cpu_check_info
+from cmk.legacy_checks.cisco_nexus_cpu import Section as CiscoNexusCpuSection
+from cmk.legacy_checks.cisco_nexus_cpu import snmp_section_cisco_nexus_cpu
 from cmk.plugins.cisco.agent_based.cisco_cpu_multiitem import (
     Section,
     snmp_section_cisco_cpu_multiitem,
@@ -82,10 +83,15 @@ def test_cisco_related_snmp_detection(
     detected: set[str],
     not_detected: set[str],
 ) -> None:
-    sections: Mapping[str, SNMPSection[StringTable, Section] | LegacyCheckDefinition] = {
+    sections: Mapping[
+        str,
+        SNMPSection[StringTable, Section]
+        | SimpleSNMPSection[StringTable, CiscoNexusCpuSection]
+        | LegacyCheckDefinition,
+    ] = {
         "cisco_cpu_multiitem": snmp_section_cisco_cpu_multiitem,
         "cisco_cpu": cisco_cpu_check_info["cisco_cpu"],
-        "cisco_nexus_cpu": cisco_nexus_cpu_check_info["cisco_nexus_cpu"],
+        "cisco_nexus_cpu": snmp_section_cisco_nexus_cpu,
     }
 
     for name in detected | not_detected:
