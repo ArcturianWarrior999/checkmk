eaf3039c71db6328c49ad1137b6617a632a4bcfb
Mathias Laurin <mathias.laurin@checkmk.com>
2026-07-14 12:58:39 +0200

    Revert "Legacy check migration: aws_elb_summary I"
    Revert submission 146011-26.19-migrate-plugins
    
    Reason for revert: test suites still broken, see previous reverts
    
    Reverted changes: /q/submissionid:146011-26.19-migrate-plugins
    
    Change-Id: Ie0192d640ed2ec4e49ce80ad3fa35def24b5714f

--- cmk/legacy_checks/aws_elb_summary.py
+++ cmk/legacy_checks/aws_elb_summary.py
@@ -3,35 +3,26 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
+# mypy: disable-error-code="type-arg"
 
-from cmk.agent_based.v2 import (
-    AgentSection,
-    CheckPlugin,
-    CheckResult,
-    DiscoveryResult,
-    Service,
-)
-from cmk.plugins.aws.lib import check_aws_elb_summary_generic, GenericAWSSection, parse_aws
+from collections.abc import Iterable
+
+from cmk.agent_based.legacy.v0_unstable import LegacyCheckDefinition
+from cmk.legacy_includes.aws import check_aws_elb_summary_generic
+from cmk.plugins.aws.lib import GenericAWSSection, parse_aws
+
+check_info = {}
 
 
-def discover_aws_elb_summary(section: GenericAWSSection) -> DiscoveryResult:
+def discover_aws_elb_summary(section: GenericAWSSection) -> Iterable[tuple[None, dict]]:
     if section:
-        yield Service()
+        yield None, {}
 
 
-def check_aws_elb_summary(section: GenericAWSSection) -> CheckResult:
-    yield from check_aws_elb_summary_generic(section)
-
-
-agent_section_aws_elb_summary = AgentSection(
+check_info["aws_elb_summary"] = LegacyCheckDefinition(
     name="aws_elb_summary",
     parse_function=parse_aws,
-)
-
-
-check_plugin_aws_elb_summary = CheckPlugin(
-    name="aws_elb_summary",
     service_name="AWS/ELB Summary",
     discovery_function=discover_aws_elb_summary,
-    check_function=check_aws_elb_summary,
+    check_function=check_aws_elb_summary_generic,
 )
