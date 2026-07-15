3e9a258ec036bfe62f2a89169ff1b007dfa2c03f
Joerg Herbel <joerg.herbel@checkmk.com>
2026-07-15 18:38:04 +0200

    Revert "Move cmk.utils.werks into cmk-werks package"
    Revert submission 146545
    
    Reason for revert: Breaks multiple tests
    
    Reverted changes: /q/submissionid:146545
    
    Change-Id: Ibdd238955cdab96748acc1730c3918e277febfed

--- cmk/utils/werks/__init__.py
+++ cmk/utils/werks/__init__.py
@@ -3,14 +3,81 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-"""Backwards-compatible re-exports to be removed."""
+"""
+Code for processing Checkmk werks. This is needed by several components,
+so it's best place is in the central library.
 
-from cmk.werks.site import (
-    COMPILED_WERKS_DIR as COMPILED_WERKS_DIR,
+We are currently in the progress of moving the werk files from nowiki syntax to
+markdown. The files written by the developers (in `.werks` folder in this repo)
+contain markdown if the filenames ends with `.md`, otherwise nowiki syntax.
+
+In order to speed up the loading of the werk files they are precompiled and
+packaged as json during release. Pydantic model Werk is used to handle the
+serializing and deserializing.
+
+But all this should be implementation details, because downstream tools should
+only handle the WerkV2 model. Old style werks are converted to markdown Werks,
+so both can be handled with a common interface.
+"""
+
+from collections.abc import Sequence
+from functools import cache
+from pathlib import Path
+
+import cmk.utils.paths
+from cmk.werks.models import WerkV3
+from cmk.werks.utils import (
+    load_precompiled_werks_file,
 )
-from cmk.werks.site import (
-    load as load,
-)
-from cmk.werks.site import (
-    load_werk_entries as load_werk_entries,
-)
+
+from .acknowledgement import load_acknowledgements, UNACKNOWLEDGED_WERKS_JSON
+
+COMPILED_WERKS_DIR = cmk.utils.paths.share_dir / "werks"
+
+
+def load(
+    *,
+    base_dir: Path | None = None,
+    unacknowledged_werks_json: Path | None = None,
+    acknowledged_werks_mk: Path | None = None,
+) -> dict[int, WerkV3]:
+    if base_dir is None:
+        base_dir = COMPILED_WERKS_DIR
+    if unacknowledged_werks_json is None:
+        unacknowledged_werks_json = UNACKNOWLEDGED_WERKS_JSON
+
+    werks: dict[int, WerkV3] = {}
+
+    unacknowledged_werks = {}
+    if unacknowledged_werks_json.exists():
+        # load unacknowledged werks that are part of the configuration
+        # and still not acknowledged by the user
+        unacknowledged_werks = load_precompiled_werks_file(unacknowledged_werks_json)
+        acknowledged_werks = load_acknowledgements(acknowledged_werks_mk=acknowledged_werks_mk)
+        unacknowledged_werks = {
+            id: werk for id, werk in unacknowledged_werks.items() if id not in acknowledged_werks
+        }
+        werks.update(unacknowledged_werks)
+
+    # load werks shipped with the version, they have to loaded after the unacknowledged werks,
+    # as they could contain more recent content
+    for file_name in [(base_dir / "werks"), *base_dir.glob("werks-*")]:
+        werks.update(load_precompiled_werks_file(file_name))
+
+    for werk_id, werk in unacknowledged_werks.items():
+        # if the werk is coming from unacknowledged_werks, then we want to present it with the first
+        # version we saw it.
+        werks[werk_id].version = werk.version
+
+    return werks
+
+
+@cache
+def load_werk_entries() -> Sequence[WerkV3]:
+    # we have a small caching inconsistency here:
+    # load() will also load unacknowledged incompatible werks of previous versions
+    # when those werks are acknowledged, they will be visible until checkmk is restarted and this
+    # cache vanishes. completely removing a werk after it was acknowledged is also not 100%
+    # expected, so this caching issue might actually be a feature.
+    werks_raw = load()
+    return list(werks_raw.values())
--- cmk/utils/werks/acknowledgement.py
+++ cmk/utils/werks/acknowledgement.py
@@ -3,23 +3,43 @@
 # This file is part of Checkmk (https://checkmk.com). It is subject to the terms and
 # conditions defined in the file COPYING, which is part of this source code package.
 
-"""Backwards-compatible re-exports to be removed."""
+from pathlib import Path
 
-from cmk.werks.site.acknowledgement import (
-    ACKNOWLEDGEMENT_PATH as ACKNOWLEDGEMENT_PATH,
-)
-from cmk.werks.site.acknowledgement import (
-    is_acknowledged as is_acknowledged,
-)
-from cmk.werks.site.acknowledgement import (
-    load_acknowledgements as load_acknowledgements,
-)
-from cmk.werks.site.acknowledgement import (
-    save_acknowledgements as save_acknowledgements,
-)
-from cmk.werks.site.acknowledgement import (
-    UNACKNOWLEDGED_WERKS_JSON as UNACKNOWLEDGED_WERKS_JSON,
-)
-from cmk.werks.site.acknowledgement import (
-    write_unacknowledged_werks as write_unacknowledged_werks,
-)
+import cmk.utils.paths
+from cmk.ccc import store
+from cmk.werks.models import WerkV3
+from cmk.werks.utils import write_precompiled_werks
+
+ACKNOWLEDGEMENT_PATH = cmk.utils.paths.var_dir / "acknowledged_werks.mk"
+UNACKNOWLEDGED_WERKS_JSON = cmk.utils.paths.var_dir / "unacknowledged_werks.json"
+
+
+def is_acknowledged(werk: WerkV3, acknowledged_werk_ids: set[int]) -> bool:
+    return werk.id in acknowledged_werk_ids
+
+
+def load_acknowledgements(
+    *,
+    acknowledged_werks_mk: Path | None = None,
+) -> set[int]:
+    if acknowledged_werks_mk is None:
+        acknowledged_werks_mk = ACKNOWLEDGEMENT_PATH
+    return set(store.load_object_from_file(acknowledged_werks_mk, default=[]))
+
+
+def save_acknowledgements(
+    acknowledged_werks: list[int],
+    *,
+    acknowledged_werks_mk: Path = ACKNOWLEDGEMENT_PATH,
+) -> None:
+    store.save_object_to_file(acknowledged_werks_mk, acknowledged_werks)
+
+
+def write_unacknowledged_werks(
+    werks: dict[int, WerkV3],
+    *,
+    unacknowledged_werks_json: Path | None = None,
+) -> None:
+    if unacknowledged_werks_json is None:
+        unacknowledged_werks_json = UNACKNOWLEDGED_WERKS_JSON
+    write_precompiled_werks(unacknowledged_werks_json, werks)
