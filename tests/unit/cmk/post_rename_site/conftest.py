a8bb00bf9d5fa7aa4cbc87dfcd8ed0020459fd39
Lars Michelsen <lars.michelsen@checkmk.com>
2026-07-21 13:18:42 +0200

    post_rename_site: drop GUI request fixtures from unit tests
    All rename actions run without a Flask context now, so the tests do
    not need the wsgi app, admin login and request context fixtures
    anymore. The only GUI related requirement left is the plug-in
    registration (host attributes, folder validators, site management,
    rulespecs), which a slim session fixture keeps providing.
    
    The test bazel targets lose their flask/wsgi dependencies accordingly.
    
    CMK-34848
    
    Change-Id: I4165d47e739819a0d08b0f0489d4477e0704f574

--- tests/unit/cmk/post_rename_site/conftest.py
+++ tests/unit/cmk/post_rename_site/conftest.py
@@ -4,91 +4,14 @@
 # conditions defined in the file COPYING, which is part of this source code package.
 from __future__ import annotations
 
-import typing
-from collections.abc import Iterator
-
 import pytest
-from flask import Flask
-from pytest_mock import MockerFixture
-from werkzeug.test import create_environ
 
-from cmk.ccc.user import UserId
 from cmk.ccc.version import Edition
-from cmk.gui import http
-from cmk.gui.config import Config
-from tests.testlib.gui.common_fixtures import (
-    create_flask_app,
-    create_wsgi_app,
-    perform_gui_cleanup_after_test,
-    perform_load_config,
-    perform_load_plugins,
-)
-from tests.testlib.gui.users import create_and_destroy_user
-from tests.testlib.gui.web_test_app import (
-    WebTestAppForCMK,
-)
-
-
-@pytest.fixture()
-def admin_auth_request(
-    with_admin: tuple[UserId, str],
-) -> typing.Generator[http.Request]:
-    # NOTE:
-    # REMOTE_USER will be omitted by `flask_app.test_client()` if only passed via an
-    # environment dict. When however a Request is passed in, the environment of the Request will
-    # not be touched.
-    user_id, _ = with_admin
-    yield http.Request({**create_environ(), "REMOTE_USER": str(user_id)})
-
-
-@pytest.fixture()
-def flask_app(
-    patch_omd_site: None,
-    use_fakeredis_client: None,
-    load_plugins: None,
-) -> Iterator[Flask]:
-    yield from create_flask_app()
-
-
-@pytest.fixture(autouse=True)
-def gui_cleanup_after_test(
-    mocker: MockerFixture,
-) -> Iterator[None]:
-    yield from perform_gui_cleanup_after_test(mocker)
-
-
-@pytest.fixture()
-def load_config(request_context: None) -> Iterator[Config]:
-    yield from perform_load_config()
+from tests.testlib.gui.common_fixtures import perform_load_plugins
 
 
 @pytest.fixture(scope="session", autouse=True)
 def load_plugins(test_edition: Edition) -> None:
+    """The rename actions rely on the GUI plug-in registration (host attributes,
+    folder validators, site management, rulespecs)"""
     perform_load_plugins(test_edition)
-
-
-@pytest.fixture()
-def request_context(flask_app: Flask) -> Iterator[None]:
-    """Empty fixture. Invokes usage of `flask_app` fixture."""
-    yield
-
-
-@pytest.fixture()
-def with_admin(load_config: Config) -> Iterator[tuple[UserId, str]]:
-    with create_and_destroy_user(automation=False, role="admin", config=load_config) as user:
-        yield user
-
-
-@pytest.fixture()
-def wsgi_app(flask_app: Flask) -> Iterator[WebTestAppForCMK]:
-    yield from create_wsgi_app(flask_app)
-
-
-@pytest.fixture(autouse=True)
-def post_rename_request_context(
-    wsgi_app: WebTestAppForCMK,
-    gui_cleanup_after_test: None,
-    admin_auth_request: http.Request,
-) -> None:
-    """This fixture registers a global htmllib.html() instance just like the regular GUI"""
-    wsgi_app.get(admin_auth_request)
